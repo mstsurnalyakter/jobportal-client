9f66026f5699c8d7436fb50435ab7b4d262263e6
mstsurnalyakter <surnalyms@gmail.com>
2024-05-10 17:20:18 +0600

    firebase setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,13 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes.jsx'
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
