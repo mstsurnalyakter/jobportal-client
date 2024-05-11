2e3a2a0f7b84c30b6c72ccdfcc07cbe41bf7062c
mstsurnalyakter <surnalyms@gmail.com>
2024-05-11 15:55:23 +0600

    axios setup

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,27 +5,25 @@ import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}`,
-  withCredentials:true
+  withCredentials: true,
 });
-
 const useAxiosSecure = () => {
-    const { logOut} = useAuth();
-    const navigate = useNavigate();
-    useEffect(()=>{
-        axiosSecure.interceptors.response.use(
-            res=>{
-                return res
-            },
-        async (error)=>{
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axiosSecure.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      async (error) => {
         if (error.response.status === 401 || error.response.status === 404) {
-          await logOut()
+          await logout();
           navigate("/login");
         }
-        return Promise.reject(error)
-            }
-        )
-    },[logOut,navigate])
-
-}
-
+        return Promise.reject(error);
+      }
+    );
+  }, [logout, navigate]);
+  return axiosSecure;
+};
 export default useAxiosSecure
