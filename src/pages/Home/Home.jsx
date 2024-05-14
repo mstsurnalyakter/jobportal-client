9045edd292514a5b41ada343d46ab94fc5cad981
mstsurnalyakter <surnalyms@gmail.com>
2024-05-14 10:22:45 +0600

    framer-motion added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,15 +2,22 @@ import Banner from "../../components/Banner"
 import JobByCategory from "../../components/JobByCategory"
 import JobProcess from "../../components/JobProcess";
 import Testimonials from "../../components/Testimonials";
-
+import { motion, useScroll, useSpring } from "framer-motion";
 
 const Home = () => {
+   const { scrollYProgress } = useScroll();
+     const scaleX = useSpring(scrollYProgress, {
+       stiffness: 100,
+       damping: 30,
+       restDelta: 0.001,
+     });
   return (
     <div className="space-y-20 dark:text-gray-100">
+      <motion.div className="progress-bar" style={{ scaleX }} />
       <Banner />
       <JobByCategory />
-      <JobProcess/>
-      <Testimonials/>
+      <JobProcess />
+      <Testimonials />
     </div>
   );
 }
